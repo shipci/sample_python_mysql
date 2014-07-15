35b3d8a8f68cb81e0e51898d01e596d31f68075a
Ragesh Krishna <ragesh@rageshkrishna.com>
2014-07-15 10:28:27 +0530

    Restore disabled test

--- test.py
+++ test.py
@@ -7,9 +7,8 @@ class test_db(unittest.TestCase):
     def testRowCount(self):
         mql = Mysql()
         mql.populate()
-        #self.failIf(mql.rowcount() != 5)
+        self.failIf(mql.rowcount() != 5)
         res = mql.call_test_proc()
-        print res
         self.failIf(res[0][0] != 'test proc')
 
 
