576d2355e170b315a767b2a2a1b3c22bd23cefd2
Ragesh Krishna <ragesh@rageshkrishna.com>
2014-07-15 10:38:29 +0530

    Infinite loop

--- test.py
+++ test.py
@@ -7,9 +7,13 @@ class test_db(unittest.TestCase):
     def testRowCount(self):
         mql = Mysql()
         mql.populate()
-        self.failIf(mql.rowcount() != 5)
-        res = mql.call_test_proc()
-        self.failIf(res[0][0] != 'test proc')
+        count = 0
+        while True:
+            count += 1
+            self.failIf(mql.rowcount() != 5)
+            res = mql.call_test_proc()
+            self.failIf(res[0][0] != 'test proc')
+            print "Executed test {0} times".format(count)
 
 
 def main():
